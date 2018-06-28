c64a96c719b934295979fa78f58e90046c2021ce
jinjin2017c <746806139@qq.com>
2018-06-29 02:42:59 +0800

    Add files via upload

---
+++ MFCTEST/mfctest/bucket_put_object.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import os,sys
+import oss2
+from time import time
+auth = oss2.Auth('LTAIomCMBttjFRJj', 'mrlRsN52kfdoqPJYCZxyaNzn1EsniF')
+bucket = oss2.Bucket(auth, 'oss-cn-beijing.aliyuncs.com','jinjinfortest')
+start_time = time()
+def percentage(consumed_bytes,total_bytes):
+    if total_bytes:
+        rate = int(100*(float(consumed_bytes)/float(total_bytes)))
+        dur_time = float(time())-float(start_time)
+        speed = (float(consumed_bytes)/float(dur_time))/(1000)
+        print('\r{0}% {1} KB/s  '.format(rate,speed), end='')
+        sys.stdout.flush()
+bucket.put_object('story.txt','a'*1024*1024*10,progress_callback = percentage)
