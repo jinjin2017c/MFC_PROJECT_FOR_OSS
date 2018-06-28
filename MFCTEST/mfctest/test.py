3a38b298dc2b82e89934ec0028f81fb3768e0a1d
jinjin2017c <746806139@qq.com>
2018-06-29 02:35:47 +0800

    Add files via upload

---
+++ MFCTEST/mfctest/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#import oss2
+
+#auth = oss2.Auth('LTAIomCMBttjFRJj', 'mrlRsN52kfdoqPJYCZxyaNzn1EsniF')
+#service = oss2.Service(auth, 'oss-cn-beijing.aliyuncs.com')
+
+#b.name for b in oss2.BucketIterator(service)
+import winsound
+winsound.Beep(600,1000)
